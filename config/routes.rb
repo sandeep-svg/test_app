6a7f727121ca55762852d04e26b9ad19cb056cb6
sandeep <sandeeparey987@gmail.com>
2020-09-30 11:50:19 +0530

    intial commit

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home' #route rooting(rooting should be the base or starting and it can be used onlt once inan controller ) controller#action
+end
